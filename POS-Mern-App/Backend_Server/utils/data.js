67976257abdbb4c0710d0d77c02264843f96594f
Rohit Gadakh <121229045+RohitGadakh@users.noreply.github.com>
2024-03-06 14:16:24 +0530

    Delete POS-Mern-App directory

--- POS-Mern-App/Backend_Server/utils/data.js
+++
@@ -1,8 +0,0 @@
-const items = [
-  { name: "Tea", price: 10, category: "drinks", image: "https://as2.ftcdn.net/v2/jpg/04/06/07/21/1000_F_406072162_Ovzz2r1tNY3m18MZ5kCAtoODmlhNBDHQ.jpg" },
-  { name: "Coffee", price: 20, category: "drinks", image: "https://as1.ftcdn.net/v2/jpg/01/26/59/52/1000_F_126595213_57BqhIpfndM4m5BiARITcwnuTwpsXnET.jpg" },
-  { name: "Chicken Biryani", price: 100, category: "rice", image: "https://t3.ftcdn.net/jpg/06/08/84/24/240_F_608842413_hdYadp6uSC7c7pq6LJew9s8gPnRSgjln.jpg" },
-  { name: "Veg Biryani", price: 80, category: "rice", image: "https://t4.ftcdn.net/jpg/01/16/92/03/240_F_116920378_usYEbGwVZKAAniu55RbFKPP6Oiej1eHm.jpg" },
-];
-
-module.exports=items;
